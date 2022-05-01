7d7b2118ef16cfd9f82f073941c3683cf34b940f
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-05-01 16:22:42 +0800

    add equatable to appexception

--- lib/error/app_exception.dart
+++ lib/error/app_exception.dart
@@ -1,4 +1,14 @@
 abstract class AppException implements Exception {
   AppException([this.message]);
   final String? message;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is AppException && other.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
 }
