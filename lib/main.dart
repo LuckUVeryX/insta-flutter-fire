d76fd3b61a3acb5c8b6fdac5f298806f29006211
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-05-01 18:05:02 +0800

    add additional safety clause for using emulators

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -16,7 +17,7 @@ void main() async {
   );
 
   const bool useFirebaseEmulator = true;
-  if (useFirebaseEmulator) {
+  if (useFirebaseEmulator && kDebugMode) {
     await _configureFirebaseEmulator();
   }
 
